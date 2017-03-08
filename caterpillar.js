2f12dc8ed09dd11c332ab15657342aa516053d2c
mfpebrizal <ftm.pebrizal@gmail.com>
2017-03-08 17:23:28 +0700

    revisi

--- caterpillar.js
+++ caterpillar.js
@@ -1,34 +1,12 @@
+const lib = require('./lib/library');
+
 // release 0
 
-let isEmpty = (arr) => {
-  return arr[0] == undefined
-}
-
-let count = (arr,countArr = 0) => {
-  return isEmpty(arr) ? countArr : count(arr.slice(1) ,countArr+1)
-}
-
-console.log(isEmpty([1,2,3,4])) // false
-console.log(count([1,2,3,4,5,6])) // 6
+console.log(lib.isEmpty([1,2,3,4])) // false
+console.log(lib.count([1,2,3,4,5,6])) // 6
 
 // release 1
 
-let head = (arr) => {
-  return arr[0]
-}
-
-let tail = (arr) => {
-  return arr.slice(1)
-}
-
-let flatten = arr => arr.reduce(
-  (acc, val) => acc.concat(
-    Array.isArray(val) ? flatten(val) : val
-  ),
-  []
-);
-
-console.log(head([1,2,3])); // 1
-console.log(tail([1,2,3])); // [2,3]
-console.log(flatten([1,[2,3]]));
-console.log(flatten([1,[2,3],4,[5,[6,7]]]));
+console.log(lib.head([1,2,3])); // 1
+console.log(lib.tail([1,2,3])); // [2,3]
+console.log(lib.flatten([1,[2,3],4,[5,[6,7]]])); //[1,2,3,4,5,6,7]
